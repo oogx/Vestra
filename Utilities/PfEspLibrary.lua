73e0314f8ef806b94aa99e4e2fe32ec7a56e3f19
Xandu <83172454+xandudiscord@users.noreply.github.com>
2022-07-24 23:43:36 +0100

    s

--- Utilities/PfEspLibrary.lua
+++ Utilities/PfEspLibrary.lua
@@ -53,7 +53,9 @@ do
             elseif (rawget(v, "updateammo")) then
                 client.hud = v    
             elseif (rawget(v, "play")) then
-                client.sound = v                        
+                client.sound = v    
+            elseif (rawget(v, "votestep")) then
+                client.hud = v                   
             end
             if rawget(v, 'player') then
                 table.insert(animations, v)
